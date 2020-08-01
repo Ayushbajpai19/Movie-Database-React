d8cc47984e036d2f6b030534bfc72a0896eec29c
Ayushbajpai19 <ayush10055@gmail.com>
2020-08-02 02:55:24 +0530

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Search({handleInput, search}){
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" 
+            placeholder="Search for a movie..." 
+            className="searchbox" 
+            onChange={handleInput} 
+            onKeyPress={search}/>
+        </section>
+    )
+}
+
+export default Search
